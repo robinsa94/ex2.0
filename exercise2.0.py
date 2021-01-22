557f4f8dd75a20342363af8b7244bc652ffaee9d
Gus <gusrobins60@gmail.com>
2021-01-21 16:33:24 -0800

    a

---
+++ exercise2.0.py
@@ -0,0 +1,3 @@
+print("Angus Robins 1/19/2021\n")
+print("At what size of a project should you start using version management software? Why?\n")
+print("Git can be used for any size software most users apply git and git hub when they have an application where they would like to work on while keeping an operating version functional while they are making changes or adding to the project. That way if they miss something or add broken code they can go back and restore the appropriate working project, or if they have more than one person working on the project were the owner would want to confirm changes before they add them to the overall program they have that ability.")
